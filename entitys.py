9010836b2cb7fbf75c7e2e1beb1ee519e81be94e
Zimo zhou <Josephzhou2017@gmail.com>
2026-05-23 18:48:20 -0700

    this

--- entitys.py
+++ entitys.py
@@ -1,10 +1,14 @@
 class Entity:
     def __init__(self, data):
         self.data = data;
+        self.resources = data["resources"]
     def attack(self):
         #where target is special condition maybe I dunno
         baseAttack = self.data["basic_attack"]
         multiplier = sum([self.data["stats"][statMultiplier["name"]] * statMultiplier["multiplier"] for statMultiplier in baseAttack["stat_multipliers"]])
+        for cost in baseAttack["costs"]:
+            for key in cost.keys():
+                self.resources[key] -= cost[key]
         a = {
                 "base_damage": baseAttack["base_damage"] * multiplier,
                 "affects": baseAttack["affects"]
--- main.py
+++ main.py
@@ -5,3 +5,4 @@ with open("Data/EntityData.json", "r") as file:
     data = file.read()
 bob = Entity(json.loads(data))
 print(bob.attack())
+print(bob.resources)
